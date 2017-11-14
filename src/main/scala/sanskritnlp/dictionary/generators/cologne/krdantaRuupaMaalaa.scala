cbbe69fb974e349a2e28e2f3ffc0feb6df6511d1
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-13 20:47:12 -0800

    Add generators

--- src/main/scala/sanskritnlp/dictionary/generators/cologne/krdantaRuupaMaalaa.scala
+++ src/main/scala/sanskritnlp/dictionary/generators/cologne/krdantaRuupaMaalaa.scala
@@ -10,7 +10,7 @@ import scala.io.Source
 object krdantaRuupaMaalaa {
   val log: Logger = LoggerFactory.getLogger(getClass.getName)
   def extractFootnotes(content: String, tagPrefix: String): (String, List[(String, String)]) = {
-    val footnotePattern = "\\[\\[?[^\\]]*\\]\\]?".r
+    val footnotePattern = "\\[\\[?[^\\]]*\\]\\]?|\\([^\\)]+\\)".r
     var contentWithFootnoteMarkers = content
     var footnotesMap = mutable.ListMap[String, String]()
     var footnoteId = 1
@@ -28,16 +28,24 @@ object krdantaRuupaMaalaa {
     (contentWithFootnoteMarkers, footnotesMap.toList)
   }
 
+  def getHeadwords(line: String): Set[String] = {
+    val cleanedLine = line.replaceAll(" *(\\{.+?\\}|\\([^\\)]+?\\)|‘.+?’|इति.+?।|१|२|३|४|५|६|७|८|९|०) *", "")
+      .replaceAll("( |-|,|;)+", "$1")
+      .replaceAll("<sup.+?sup>", "")
+    cleanedLine.split("[ ;,]+").map(_.trim).filterNot(_.isEmpty).toSet
+  }
+
   def makeBabylon(): Unit = {
     val baseTsvStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/kRdanta-rUpa-mAlA/mUlam/kRdanta-rUpa-mAlA.tsv"
     val baseTsvSource = Source.fromFile(name = baseTsvStr, enc = StandardCharsets.UTF_8.name())
     baseTsvSource.getLines.take(10).map(_.split("\t")).zipWithIndex.
       foreach({case (Array(key: String, content: String), index: Int) =>
       var (contentWithFootnoteMarkers, footnotesMap) = extractFootnotes(content, index.toString)
-      // Let's not have daNDa-s within parantheses to avoid bad line breaks later.
-      contentWithFootnoteMarkers = contentWithFootnoteMarkers.replaceAll("\\(([^)]*)।([^)]*)\\)", "($1,$2)")
+
+
       // We don't directly split below to accommodate the [^०-९] condition.
       val finalContentItems = contentWithFootnoteMarkers.replace("\\n\\n", "").replaceAll("([^०-९]। |;)", "$1<BR><BR>").split("<BR><BR>").filterNot(_.isEmpty)
+      val headwords = Seq(key) ++ finalContentItems.filter(_.endsWith(";")).map(getHeadwords).flatten.toSeq
       val finalContentItemsHtml =  finalContentItems.map(x => s"""<li>$x</li>""")
       val footnoteItems = footnotesMap.sortBy(_._1).map({ case (fullFootnoteId: String, content: String) => //noinspection RedundantBlock
       {
@@ -45,7 +53,7 @@ object krdantaRuupaMaalaa {
         s"""<li id=\"foot_${fullFootnoteId}\"><a href=\"#ref_${fullFootnoteId}\">↑</a> $content</li>"""
       }})
       val footnoteHtml = if (footnoteItems.isEmpty) "" else s"<p>प्रासङ्गिक्यः <ul>${footnoteItems.mkString(" ")}</ul></p>"
-      val finalContents = s"<ul>${finalContentItemsHtml.mkString(" ")}</ul>$footnoteHtml"
+      val finalContents = s"${headwords.mkString("|")}\n<ul>${finalContentItemsHtml.mkString(" ")}</ul>$footnoteHtml"
 //      log.debug(index.toString)
 //      log.debug(contentWithFootnoteMarkers)
       log.debug(finalContents)
