65d3f5398e9e62f66b55e127f2c6f205e5a44847
vishvAsaH <vishvas.vasuki@gmail.com>
2017-12-08 13:10:43 -0800

    add kRShNamAchArya dhAtupATha files

--- src/main/scala/sanskritnlp/dictionary/generators/krshnaDhaatupaatha.scala
+++ src/main/scala/sanskritnlp/dictionary/generators/krshnaDhaatupaatha.scala
@@ -1,32 +1,26 @@
 package sanskritnlp.dictionary.generators
 
 import java.io.{File, PrintWriter}
-import java.nio.charset.StandardCharsets
 
-import org.json4s.{DefaultFormats, Formats}
 import org.json4s.native.Serialization
+import org.json4s.{DefaultFormats, Formats}
 import org.slf4j.{Logger, LoggerFactory}
-import sanskritnlp.transliteration.harvardKyoto
 
-import scala.collection.mutable
-import scala.io.Source
-
-object pushpaaKrdanta {
+object krshnaDhaatupaatha {
   private val log: Logger = LoggerFactory.getLogger(getClass.getName)
 
   def tsvToJsonBabylon(): Unit = {
     implicit val formats: Formats = DefaultFormats
-    val infileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/puShpA-ArdhadhAtuka/mUlam/ArdhadhAtuka-kRdanta-koshaH.tsv"
-    val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/puShpA-ArdhadhAtuka/puShpA-ArdhadhAtuka.babylon"
+    val infileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/dhAtu-pATha-kRShNamAchArya/mUlam/dhAtu-pATha-kRShNamAchArya.tsv"
+    val outfileStr = "/home/vvasuki/stardict-sanskrit/sa-vyAkaraNa/dhAtu-pATha-kRShNamAchArya/pdhAtu-pATha-kRShNamAchArya.babylon"
     val src = utils.tsvToMapIterator(infileStr)
     val outFileObj = new File(outfileStr)
     new File(outFileObj.getParent).mkdirs
     val destination = new PrintWriter(outFileObj)
 
-    src.map(valueMap=> valueMap.filter(tuple2 => (tuple2._2.trim != "" && tuple2._1 != "क्रमाङ्कः"))
-      .mapValues(_.split("/").map(_.trim)))
+    src.map(valueMap=> valueMap.mapValues(_.split("/").map(_.trim)))
       .foreach(valueMap => {
-      val headers = valueMap.values.flatten.toList.distinct.filterNot(_.isEmpty)
+      val headers = valueMap.filterKeys(key => Set("धातुः", "मूलधातुः", "धात्वर्थः", "रूपम्").contains(key)).values.flatten.filter(!_.isEmpty).toList.distinct
       val headersLine = headers.mkString("|")
       val meaningLine =  Serialization.writePretty(valueMap).replace("\n", "<BR>")
       destination.println(headersLine)
