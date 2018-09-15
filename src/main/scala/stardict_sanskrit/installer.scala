98c45264cb3f2cf5f0456056b4bd18f777c7a115
vvasuki <vishvas.vasuki@gmail.com>
2018-09-15 12:55:30 -0700

    Download functionality successful

--- src/main/scala/stardict_sanskrit/installer.scala
+++ src/main/scala/stardict_sanskrit/installer.scala
@@ -1,36 +1,39 @@
 package stardict_sanskrit
+import java.io.File
+
 import akka.pattern.ask
-import java.nio.file.Paths
+import java.nio.file.{Files, Paths}
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, IOResult}
 import org.slf4j.{Logger, LoggerFactory}
 import sanskrit_coders.{RichHttpClient, Utils}
 import akka.http.scaladsl.model._
-import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.Timeout
 
 import scala.concurrent.Future
-import scala.io.Source
+import scala.reflect.io.File
 import scala.util.{Failure, Success}
 
 
 case class Dictionary(dictTarUrl: String, destinationFolder: String)
 
 
-case class DictIndex(indexUrl: String, var downloadPath: String = "", var dictTarUrls: List[String] = List()) {
-  downloadPath = indexUrl.replaceAllLiterally("https://raw.githubusercontent.com/", "").replaceAllLiterally("master/", "").replaceAllLiterally("tars/tars.MD", "")
+case class DictIndex(indexUrl: String, downloadPathPrefix: String, var downloadPath: String = "", var dictTarUrls: List[String] = List()) {
+  downloadPath = downloadPathPrefix + "/" + indexUrl.replaceAllLiterally("https://raw.githubusercontent.com/", "").replaceAllLiterally("master/", "").replaceAllLiterally("tars/tars.MD", "")
   dictTarUrls = DictIndex.getUrlsFromIndexMd(url=indexUrl).toList
 
-  val dictionaries  = dictTarUrls.map(dictTarUrl => Dictionary(dictTarUrl=dictTarUrl, destinationFolder = downloadPath))
+  val dictionaries: List[Dictionary] = dictTarUrls.map(dictTarUrl => Dictionary(dictTarUrl=dictTarUrl, destinationFolder = downloadPath))
 //
 //  override def toString: String = s"indexUrl: ${indexUrl}\ndownloadPath: ${downloadPath}\ndictTarUrls: ${dictTarUrls.mkString("\n")}"
 }
 
 object DictIndex {
   def getUrlsFromIndexMd(url: String): Array[String] = {
+    import scala.io.Source
     Source.fromURL(url).mkString.split("\n").map(_.replaceAll("[<>]", ""))
   }
 }
@@ -48,9 +51,16 @@ class InstallerActor extends Actor with ActorLogging {
   def receive: PartialFunction[Any, Unit] = {
     case dict: Dictionary => {
       log.debug(dict.toString)
-      val fileSink = FileIO.toPath(Paths.get(dict.destinationFolder + "/" +  dict.dictTarUrl.split("/").last))
-      redirectingClient(HttpRequest(uri = dict.dictTarUrl)).map(_.entity.dataBytes.to(fileSink).run()).pipeTo(sender())
-      // TODO: To be continued.
+      val destinationPath = Paths.get(dict.destinationFolder,  dict.dictTarUrl.split("/").last)
+      assert(new java.io.File(dict.destinationFolder).mkdirs())
+//      Files.createFile(destinationPath)
+      val fileSink = FileIO.toPath(destinationPath)
+      log.debug(fileSink.toString())
+      val httpResponseFuture = redirectingClient(HttpRequest(uri = dict.dictTarUrl))
+      val ioResultFuture = httpResponseFuture.flatMap(response => {
+        response.entity.dataBytes.runWith(fileSink)
+      })
+      ioResultFuture.flatMap(ioResult => Future.fromTry(ioResult.status)).pipeTo(sender())
     }
   }
 }
@@ -62,14 +72,20 @@ object installer {
 
   def install(destination: String, indexOfIndicesUrl: String): Unit = {
     val indices = DictIndex.getUrlsFromIndexMd(indexOfIndicesUrl)
+    val dictionaries = indices.map(new DictIndex(_, downloadPathPrefix = destination)).flatMap(index => index.dictionaries).take(1)
+
     val installerActorRef = system.actorOf(Props[InstallerActor], "installerActor")
     // Actor ask timeout
     implicit val timeout: Timeout = Timeout(10, TimeUnit.MINUTES)
     import scala.concurrent.ExecutionContext.Implicits.global
 
     //    log.debug(indices.mkString(","))
-    val resultFutures = indices.map(new DictIndex(_)).map(index => index.dictionaries).flatten.map( dictionary =>  ask(installerActorRef, dictionary) ).take(1)
-    val futureOfResults = Utils.getFutureOfTrys(resultFutures)
+    val resultFutureList = dictionaries.map( dictionary =>  ask(installerActorRef, dictionary) )
+    val futureOfResults: Unit = Utils.getFutureOfTrys(resultFutureList). onComplete {
+      case Success(resultList) =>
+        log.info(resultList.mkString("\n"))
+        system.terminate()
+    }
 /*
 .foreach (someTry => someTry match {
         case Success(value) => log.info("All done!")
