252a6dfa1c0b58deb646dc914deb8e7e1d281249
vvasuki <vishvas.vasuki@gmail.com>
2019-05-14 15:34:12 -0700

    Auto build

--- bin/bash/tsv_to_stardict.sh
+++ bin/bash/tsv_to_stardict.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PATH_TO_SANSKRITNLPJAVA=~/sanskritnlpjava
-BABYLON_BINARY=${BABYLON_BINARY:-babylon}
+BABYLON_BINARY=${BABYLON_BINARY:-/usr/lib/stardict-tools/babylon}
 PATH_TO_JARS=~/dict-tools
 java -cp "$PATH_TO_JARS/bin/artifacts/dict-tools.jar" stardict_sanskrit.commandInterface makeStardict $1 $BABYLON_BINARY
