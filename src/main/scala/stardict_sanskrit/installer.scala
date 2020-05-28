c1dc85599aca3219eebdc2f9541aa7855e90e456
vvasuki <vishvas.vasuki@gmail.com>
2020-05-28 16:27:15 +0530

    CU

--- src/main/scala/stardict_sanskrit/installer.scala
+++ src/main/scala/stardict_sanskrit/installer.scala
@@ -64,9 +64,9 @@ class InstallerActor extends Actor with ActorLogging {
       log.info(dict.toString)
       val destinationTarPath = Paths.get(dict.destinationFolder, dict.dictName, dict.tarFilename)
       val dictionaryFolder = new java.io.File(destinationTarPath.getParent.toString)
-      val doesDictExist = dictionaryFolder.exists() && dictionaryFolder.listFiles().length > 3 && dictionaryFolder.listFiles().find(_.getName.endsWith("ifo")) != None
-      val localDictFileNewer = doesDictExist && dictionaryFolder.listFiles().filter(_.getName.endsWith("ifo")).head.lastModified() > dict.timestamp
-      if (!overwrite && localDictFileNewer) {
+      val doesDictExist = dictionaryFolder.exists() && dictionaryFolder.listFiles().length >= 3 && dictionaryFolder.listFiles().find(_.getName.endsWith("ifo")) != None
+      val localDictFileNewer = doesDictExist && (dictionaryFolder.listFiles().filter(_.getName.endsWith("ifo")).head.lastModified() > dict.timestamp || dict.timestamp == 0)
+      if (!overwrite && (localDictFileNewer)) {
         log.warning(s"Skipping pre-existing seemingly newer $dict")
         Future.fromTry(Success(s"Dict already exists: $dict")).pipeTo(sender())
       } else {
