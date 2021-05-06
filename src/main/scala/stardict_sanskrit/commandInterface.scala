159c2efaa750639fbda9df374331bb14edd5e6d0
vvasuki <vishvas.vasuki@gmail.com>
2021-05-06 06:37:37 +0530

    CU

--- src/main/scala/stardict_sanskrit/commandInterface.scala
+++ src/main/scala/stardict_sanskrit/commandInterface.scala
@@ -141,8 +141,9 @@ object commandInterface {
         commandConfig.mode.get match {
           case "install" => installer.install(destination = commandConfig.destinationPath.get, indexOfIndicesUrl = commandConfig.dictRepoIndexUrl.get, overwrite = commandConfig.overwrite.getOrElse(false))
           case "makeIndicStardictTar" => batchProcessor.makeIndicStardictTar(dictPattern = commandConfig.dictPattern.get, babylonBinary =  commandConfig.babylonBinary.get, overwrite = commandConfig.overwrite.getOrElse(false), tarBaseUrl =  commandConfig.urlBase.get, githubToken = commandConfig.githubToken)
-            batchProcessor.makeSlobs(dictPattern = commandConfig.dictPattern.get, overwrite = commandConfig.overwrite.getOrElse(false), baseUrl =  commandConfig.urlBase.get, githubToken = commandConfig.githubToken, babylonBinary = commandConfig.babylonBinary.get)  
-            batchProcessor.makePerHeadwordMdFiles(dictPattern = commandConfig.dictPattern.get, tarBaseUrl =  commandConfig.urlBase.get, githubToken = commandConfig.githubToken)
+            batchProcessor.makeSlobs(dictPattern = commandConfig.dictPattern.get, overwrite = commandConfig.overwrite.getOrElse(false), baseUrl =  commandConfig.urlBase.get, githubToken = commandConfig.githubToken, babylonBinary = commandConfig.babylonBinary.get)
+            // The below result in too slow pushes leading to failed builds like https://github.com/indic-dict/stardict-sanskrit/runs/2511181643.
+//            batchProcessor.makePerHeadwordMdFiles(dictPattern = commandConfig.dictPattern.get, tarBaseUrl =  commandConfig.urlBase.get, githubToken = commandConfig.githubToken)
           case unknownCommand => log.error(s"Do not recognize $unknownCommand")
         } // successful parse case ends
       case _ =>
